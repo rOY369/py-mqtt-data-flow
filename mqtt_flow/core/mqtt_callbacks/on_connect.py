0c664ce046ed5c4ac36c3d3cc37b4cdfc2a142d1
roy <dishant.arora1996@gmail.com>
2024-03-12 23:37:49 +0530

    reverted back log level

--- mqtt_flow/core/mqtt_callbacks/on_connect.py
+++ mqtt_flow/core/mqtt_callbacks/on_connect.py
@@ -40,7 +40,7 @@ class OnConnectCallback:
                 f"MQTT client {client._client_id} connected with result code {rc}"
             )
             for topic in instance.sub_topics:
-                logger.info(f"{client._client_id} Subscribing to {topic}")
+                logger.debug(f"{client._client_id} Subscribing to {topic}")
                 client.subscribe(topic)
 
         return on_connect
