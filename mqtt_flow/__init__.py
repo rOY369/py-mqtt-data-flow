8aecb3487c6830ead23a57137e90b29f67f6e78c
roy <dishant.arora1996@gmail.com>
2024-03-15 18:40:48 +0530

    trying out new logger mechanism

--- mqtt_flow/__init__.py
+++ mqtt_flow/__init__.py
@@ -1,5 +1,6 @@
 from mqtt_flow.core.mqtt_flow import MQTTFlow
 from mqtt_flow.config.loader import MQTTConfigLoader
 from mqtt_flow.mqtt_lib.mqtt_client import MQTTClient
+from mqtt_flow.utils.helpers import set_logger
 
 MQTTCustom = MQTTClient
