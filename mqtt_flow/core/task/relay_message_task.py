d6f057753740e6b672efb7386ff17671cd1e81ab
roy <dishant.arora1996@gmail.com>
2024-04-23 17:59:00 +0530

    optional logging for message relay task

--- mqtt_flow/core/task/relay_message_task.py
+++ mqtt_flow/core/task/relay_message_task.py
@@ -1,13 +1,16 @@
 from mqtt_flow.core.task.flow_task import MQTTFlowTask
 from mqtt_flow.utils.helpers import format_topic
+from mqtt_flow.utils.helpers import get_logger
 
 
 class RelayMessage(MQTTFlowTask):
     def __init__(self, topic, payload, userdata, task_config):
+        self.logger = get_logger("relay_message")
         super().__init__(topic, payload, userdata, task_config)
         self.client_to_publish = self.task_config.get("client_to_publish")
         self.topic_to_publish = self.task_config.get("topic_to_publish")
         self.persist = self.task_config.get("persist", False)
+        self.log = self.task_config.get("log", False)
 
         if not self.topic_to_publish:
             self.topic_formatters = self.task_config.get(
@@ -30,3 +33,8 @@ class RelayMessage(MQTTFlowTask):
         self.publish_message(
             self.client_to_publish, topic, payload, persist=self.persist
         )
+
+        if self.log:
+            self.logger.info(
+                f"Message relayed to client {self.client_to_publish}: {topic} -> {payload}"
+            )
