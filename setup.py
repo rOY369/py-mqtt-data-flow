2e61c014fd00aff04c0e53352b6c0c192cb84de9
roy <dishant.arora1996@gmail.com>
2024-06-05 17:21:42 +0530

    changed mqtt flow version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="mqtt_flow",
-    version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
-    description="A scalable, modular framework for MQTT message processing in Python.",
+    version="1.0.0",
+    author="Dishant Arora",
+    author_email="dishant.arora1996@gmail.com",
+    description="A scalable & modular framework for MQTT message processing in Python.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/rOY369/py-mqtt-data-flow",
@@ -14,7 +14,7 @@ setup(
         "paho-mqtt==1.6.1",
         "PyYAML>=5.4",
         "retry",
-        "persistqueue"
+        "persistqueue",
         # Add other dependencies as needed
     ],
     extras_require={
