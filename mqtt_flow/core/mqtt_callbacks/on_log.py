4df4a19d6fbe3acde628badb3e6ddeca7ebd96a1
roy <dishant.arora1996@gmail.com>
2024-09-14 00:11:05 +0530

    minor patch

--- mqtt_flow/core/mqtt_callbacks/on_log.py
+++ mqtt_flow/core/mqtt_callbacks/on_log.py
@@ -1,7 +1,2 @@
-from mqtt_flow.utils.helpers import get_logger
-
-logger = get_logger("on_log_mqtt_callback")
-
-
 def on_log(client, userdata, level, buf):
-    logger.info(f"client {client._client_id} on_log: {buf}")
+    print(f"client {client._client_id} on_log: {buf}")
