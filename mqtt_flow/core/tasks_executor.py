2a1c0f3a16bd3a15ae44cc0fe19badec4bcba218
roy <dishant.arora1996@gmail.com>
2024-12-29 14:37:38 +0530

    minor patch

--- mqtt_flow/core/tasks_executor.py
+++ mqtt_flow/core/tasks_executor.py
@@ -5,9 +5,11 @@ from mqtt_flow.core.executor_pools import (
     SequentialPool,
 )
 from mqtt_flow.utils.helpers import get_logger
+import time
 
 
 class TasksExecutor:
+    DEFAULT_EXECUTION_RATE_LIMIT_PER_SECOND = 10
     POOL_TYPES = {
         "simple_thread": SimpleThreadPool,
         "thread": ThreadPool,
@@ -29,25 +31,47 @@ class TasksExecutor:
             pools[pool_name] = self.POOL_TYPES[pool_type](pool_config)
         return pools
 
-    def consume_task_queue(self, task_queue, pool):
+    def consume_task_queue(
+        self, task_queue, pool, execution_rate_limit_per_second
+    ):
+        # Calculate the time interval per task
+        time_per_task = 1 / execution_rate_limit_per_second
+        last_execution_time = time.time()
+
         while True:
             try:
                 task = task_queue.get()
                 self.logger.debug(f"Executing Task {task}")
                 if pool.resource_available:
                     pool.submit(task.process)
+
+                # Enforce rate limiting
+                current_time = time.time()
+                elapsed_time = current_time - last_execution_time
+                if elapsed_time < time_per_task:
+                    time.sleep(time_per_task - elapsed_time)
+
+                last_execution_time = time.time()
+
             except Exception:
                 self.logger.exception("Exception in Task Consumer")
 
     def start(self):
         for task_queue_name, task_queue in self.tasks_queues.items():
-
+            pool = None
             for queue_config in self.queues_config:
                 if queue_config.get("name") == task_queue_name:
                     pool_name = queue_config.get("pool")
                     pool = self._pools[pool_name]
+                    execution_rate_limit_per_second = queue_config.get(
+                        "execution_rate_limit_per_second",
+                        self.DEFAULT_EXECUTION_RATE_LIMIT_PER_SECOND,
+                    )
+                    break
 
-            task_queue_thread = threading.Thread(
-                target=self.consume_task_queue, args=(task_queue, pool)
-            )
-            task_queue_thread.start()
+            if pool is not None:
+                task_queue_thread = threading.Thread(
+                    target=self.consume_task_queue,
+                    args=(task_queue, pool, execution_rate_limit_per_second),
+                )
+                task_queue_thread.start()
