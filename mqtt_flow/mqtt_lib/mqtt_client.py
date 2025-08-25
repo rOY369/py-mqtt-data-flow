16450aa4da67db89b7c83b06a9d1f711d048e697
rOY369 <dishant.arora1996@gmail.com>
2025-08-25 12:29:13 +0530

    Update MQTTClient SSL context to enforce TLS 1.3 exclusively, removing fallback for older versions and cleaning up commented code for clarity.

--- mqtt_flow/mqtt_lib/mqtt_client.py
+++ mqtt_flow/mqtt_lib/mqtt_client.py
@@ -223,13 +223,13 @@ class MQTTClient:
         ssl_context = ssl.create_default_context()
         # ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_3)
 
-        try:
-            ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
-            ssl_context.maximum_version = ssl.TLSVersion.TLSv1_3
-        except AttributeError as e:
-            # Very old Python/OpenSSL fallback
-            self.log.warning(f"Using old Python/OpenSSL fallback : {e}")
-            ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        # try:
+        # ssl_context.minimum_version = ssl.TLSVersion.TLSv1_2
+        ssl_context.minimum_version = ssl.TLSVersion.TLSv1_3
+        # except AttributeError as e:
+        #     # Very old Python/OpenSSL fallback
+        #     self.log.warning(f"Using old Python/OpenSSL fallback : {e}")
+        #     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
 
         if alpn_protocol:
             ssl_context.set_alpn_protocols([alpn_protocol])
@@ -258,6 +258,7 @@ class MQTTClient:
                 self.ssl_config.get("key"),
             )
             self.client.tls_set_context(context=ssl_context)
+            self.client.tls_set(ssl.TLSVersion.TLSv1_3)
         if self.will_topic and self.will_payload:
             self.client.will_set(self.will_topic, self.will_payload)
         self.client.reconnect_delay_set(min_delay=1, max_delay=self.max_reconnect_delay)
